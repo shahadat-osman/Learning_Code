70e68a6f0ade57af87a8dae6d67d96c8b0a49096
shahadat-mso <sahadat.mso@gmail.com>
2022-03-24 20:55:16 +0600

    mod

--- w3schools/ex1.c
+++ w3schools/ex1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>  //header file library
 int main()          //this is main function
 {
-    printf("Hello World");
+    printf("Hello World");      //printf is a function used to show output/print text. i.e This line will show Hello World
     return 0;
 }
