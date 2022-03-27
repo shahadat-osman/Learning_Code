f30d3c5de51f509b3ec267ae8d9b7d21e1ab14b9
shahadat-mso <sahadat.mso@gmail.com>
2022-03-27 23:27:21 +0600

    mod

--- w3schools/ex4.c
+++ w3schools/ex4.c
@@ -2,7 +2,10 @@
 
 int main()
 {
-    printf("Hello World!\nI am learning C.\nAnd it's awesome!\n");
+    printf("1\nHello World!\nI am learning C.\nAnd it's awesome!\n");         // HERE, "\n" breaks the line
+    printf("2\nHello\tWorld!\n");                                             // HERE, "\t" creates a tab
+    printf("3\nHello\\World!\n");                                             // HERE, "\\" inserts a backslash(\) character
+    printf("4\nHello \"World!\"\n");                                          // HERE, \" inserts a double quote character.
 
     return 0;
 }
