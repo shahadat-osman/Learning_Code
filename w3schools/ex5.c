2ba5b34725b1e78b4bd7ea190709766fb9ce2e25
shahadat-mso <sahadat.mso@gmail.com>
2022-03-28 00:40:08 +0600

    mod

--- w3schools/ex5.c
+++ w3schools/ex5.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main()
-{
-    int num=20;         //syntax of variable declare: type variableName = value;
-    double dec=34.5;
-    char letter='A';
+{                           //syntax of variable declare: type variableName = value;
+    int num=20;             //Integer (whole number)
+    double dec=34.5;        //Floating point number
+    char letter='A';        //Character
 
-    printf("integer=%d\ndecimal=%lf\nAlphabet=%c\n", num,dec,letter);  //Here, %d, %f, %c are format specifiers
-
+    printf("%d\n", num);    //Here, %d, %f, %c are format specifiers
+    printf("%f\n", dec);
+    printf("%c\n", letter);
     return 0;
 }
