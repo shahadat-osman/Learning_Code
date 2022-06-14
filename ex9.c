80897e3a882235dd8ee4708507ea046d9e2893b6
shahadat-mso <sahadat.mso@gmail.com>
2022-06-14 22:30:06 +0600

    init

---
+++ ex9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a= 70;
+    float b=5.6;
+    char c='S';
+    char d='s';
+
+    printf("%d\n",a);
+    printf("%0.2lf\n",b);
+    printf("%c\n", c);
+    printf("%c\n", d);
+
+    return 0;
+
+}
