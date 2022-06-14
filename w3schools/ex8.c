3d81f5823136e17ef40ff16e4039e0c90186f7b3
shahadat-mso <sahadat.mso@gmail.com>
2022-06-14 18:40:46 +0600

    init

---
+++ w3schools/ex8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int myNum=15;
+    double myFloatNum=5.99;
+    char myLetter='S';
+
+    printf("%d\n", myNum);
+    printf("%.2f\n", myFloatNum);
+    printf("%c\n", myLetter);
+
+    return 0;
+}
