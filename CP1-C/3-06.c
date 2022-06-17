e0c080ea47f8493202fd6c711d858caf44cb0f77
shahadat-mso <sahadat.mso@gmail.com>
2022-06-17 22:23:58 +0600

    init

---
+++ CP1-C/3-06.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n=2;
+
+    if (n<30)
+    {
+        printf("n is less than 30.");
+    }
+    else if(n<50)
+    {
+        printf("n is less than 50.");
+    }
+    return 0;
+}
---
+++ CP1-C/3-07.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n=3;
+    if(n<30)
+    {
+        printf("n is less than 30.");
+    }
+    if(n<50)
+    {
+        printf("n is less than 50");
+    }
+    return 0;
+}
