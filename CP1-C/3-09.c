b53de84a3e3e8df35657c2ccabce311f4db15969
shahadat-mso <sahadat.mso@gmail.com>
2022-06-18 01:32:18 +0600

    init

---
+++ CP1-C/3-09.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int number=8;
+
+    if(number%2==0)
+    {
+        printf("The number is even");
+    }
+    else
+    {
+        printf("The number is odd");
+    }
+    return 0;
+}
