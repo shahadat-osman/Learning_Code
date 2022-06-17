8252e8144855c22d8eca333d7e0943fdf2a0ed8e
shahadat-mso <sahadat.mso@gmail.com>
2022-06-18 01:32:57 +0600

    init

---
+++ CP1-C/3-09test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=3, b=11,quotient,remainder,multiply;
+    quotient=b/a;
+    multiply=quotient*a;
+    remainder=b-multiply;
+    printf("Ans is %d", remainder);
+
+
+
+
+
+    return 0;
+}
