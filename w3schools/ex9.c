f2ff25b9abc7a84debbbc83c80836cd35fed6615
shahadat-mso <sahadat.mso@gmail.com>
2022-06-17 21:06:11 +0600

    mod

--- w3schools/ex9.c
+++ w3schools/ex9.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 int  main()
 {
-    int x=2, y=5;
-    x*=3;
+    int x=2, y=6;
+
+    printf("X=%d, Y=%d\n", x,y);
 
     if (x>y)
     {
         printf("X is greater than Y");
+    }
+    else if (x==y)
+    {
+        printf("X is equal to Y");
+    }
+    else
+    {
+        printf("Y is greater than X");
     }
 
     return 0;
