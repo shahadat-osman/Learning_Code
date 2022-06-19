844ad53768f6dad61ccbf036f9d975af1c44f444
shahadat-mso <sahadat.mso@gmail.com>
2022-06-19 21:05:42 +0600

    init

---
+++ w3s/C/ex13.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int day;
+    printf("Input a number: ");
+    scanf("%i", &day);
+
+    switch (day)
+    {
+        case 1:
+            printf("Saturday");
+            break;
+        case 2:
+            printf("Sunday");
+            break;
+        case 3:
+            printf("Monday");
+            break;
+        case 4:
+            printf("Tuesday");
+            break;
+        case 5:
+            printf("Wednesday");
+            break;
+        case 6:
+            printf("Thursday");
+            break;
+        case 7:
+            printf("Friday");
+            break;
+        default:
+            printf("Other day");
+    }
+    return 0;
+
+}
