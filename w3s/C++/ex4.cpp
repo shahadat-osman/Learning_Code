c93266ce32bb8f34b9fda82a32a27ea4d7cd5ec2
shahadat-mso <sahadat.mso@gmail.com>
2022-06-23 01:03:01 +0600

    mod

---
+++ w3s/C++/ex4.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string txt="ABCDEFGHIJKLMNOPQRSTUVWXYZ1234";
+    cout<< "The length of the string is: "<<txt.length()<<endl;
+
+    return 0;
+}
