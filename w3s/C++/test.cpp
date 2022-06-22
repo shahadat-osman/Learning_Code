3c6a4ad617571a6c361ba3e55e2d154d28dd66fc
shahadat-mso <sahadat.mso@gmail.com>
2022-06-22 22:18:53 +0600

    mod

--- w3s/C++/test.cpp
+++ w3s/C++/test.cpp
@@ -4,12 +4,13 @@ using namespace std;
 int main()
 {
     int x, y;
-    int sum=x+y;
-    cout << "Type a number: ";
-    cin >> x;
-    cout << "Type another number: ";
-    cin >> y;
-    cout << "Sum is: " << sum;
+    int sum;
+    cout<< "Type a number: ";
+    cin>> x;
+    cout<< "Type another number: ";
+    cin>> y;
+    sum = x+y;
+    cout<< "Sum is: "<<sum;
 
     return 0;
 }
