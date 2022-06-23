21c2fdce7cf8b8150be66a65b2f7da10bf0b04f5
shahadat-mso <sahadat.mso@gmail.com>
2022-06-23 13:20:40 +0600

    init

---
+++ w3s/C++/ex10.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    cout<<sqrt(144)<<endl;
+
+
+    return 0;
+}
---
+++ w3s/C++/ex9.cpp
@@ -0,0 +1,11 @@
+//max(x,y) and min(x,y)
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<max(5,11)<<endl;
+    cout<<min(12,11)<<endl;
+
+    return 0;
+}
