e6ec882c1fae828284598ff9b1e7fa1f85c5a86f
shahadat-mso <sahadat.mso@gmail.com>
2022-06-23 12:57:00 +0600

    init

---
+++ w3s/C++/ex7.cpp
@@ -0,0 +1,12 @@
+//user input strings
+#include<iostream>
+using namespace std;
+int main()
+{
+    string Name;
+    cout<<"Type your first name: ";
+    cin>>Name;
+    cout<<"Your name is: "<<Name<<"\n";
+
+    return 0;
+}
