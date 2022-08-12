59cdf056f7f3bbe67ff7d3885e28f42fa2199bd7
shahadat-mso <sahadat.mso@gmail.com>
2022-08-12 18:30:49 +0600

    mod

--- w3s/C/rough.c
+++ w3s/C/rough.c
@@ -1,29 +1,50 @@
 #include<stdio.h>
 int main()
 {
-    int num=0;
+    int n, p, num=0;
 
-    // printf("Enter a Number:");
-    // scanf("%d", &num);
-
-    /* while(num<=5)
-    {
-    printf("\n%d Hello Tuhin\n", num);
+   //while loop
+    while (num<=5)
+   {
+    printf("%d Hello World\n", num);
     num++;
-    }
-    printf("\n"); */
-    
-    /* do
-    {
-        printf("Hello World..%d\n", num);
-        num++;
-    }
-    while(num<=10); */
+   }
 
-    for (num=0; num<=10; num++)
+   //do loop
+   do
+   {
+    printf("Hello Tech People %d\n", num);
+    num=num+2;
+   }
+   while (num<=10);
+
+   //for loop
+    for (n=1; n<=10; n=n+2)
+   {
+   printf("Hello Programmers! %d\n", n);
+   }
+
+    //for loop with break
+    int i;
+   for (i=0; i<10; i++)
+   {
+    if(i==4) 
     {
-        printf("\n%d Hello World.\n\n", num);
+        break;
     }
+    printf("%d Hello Coder\n", i);
+   }
+
+    //for loop with continue
+   for (p=1; p<=10; p++)
+   {
+    if (p==6)
+    {
+        printf("%d Not Allowed\n", p);
+        continue;
+    }
+    printf("%d Hello Shahadat\n", p);
+   }
 
     return 0;
 }
