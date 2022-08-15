adc7a132bc704590403be0cb172f08fc42d7f655
shahadat-mso <sahadat.mso@gmail.com>
2022-08-16 01:24:20 +0600

    mod

--- TY-C/1.8-T1.c
+++ TY-C/1.8-T1.c
@@ -1,11 +1,22 @@
+//using sqrt in assignment statement and simply used to printf function.
 #include<stdio.h>
 #include<math.h>
+int main2();
+
 int main()
 {
     double answer;
 
     answer=sqrt(10.0);
-    printf("%f", answer);
+    printf("%f\n", answer);
+
+    main2();
+
+    return 0;
+}
+int main2()
+{
+    printf("%f", sqrt(10.0));
 
     return 0;
 }
