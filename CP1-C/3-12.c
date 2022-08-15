4b56c4d6118b194665bc306588405644c86515df
shahadat-mso <sahadat.mso@gmail.com>
2022-08-15 12:18:28 +0600

    mod

---
+++ CP1-C/3-12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a=50;
+
+    if(a>=0 || a<=10)
+    {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+
+    return 0;
+}
