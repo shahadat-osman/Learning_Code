e14d18190b21bbe6346c8ebc6b776cca244f8ea8
shahadat-mso <sahadat.mso@gmail.com>
2022-08-16 22:00:07 +0600

    mod

---
+++ TY-C/1.10-01.c
@@ -0,0 +1,14 @@
+//The moon's gravity is about 17 percent of Earth's. write a program that allows you to enter your weight and computes your effective weight on the moon.
+#include<stdio.h>
+int main()
+{
+    float weight, m_weight, conversion=17/100;
+
+    printf("Enter weight on earth:");
+    scanf("%f", &weight);
+    
+    m_weight=weight*conversion;
+    printf("Moon's weight is: %0.2f", m_weight);
+
+    return 0;
+}
