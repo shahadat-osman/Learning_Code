f753b78fd9c8fc6f2947190a61246f1a23b300d6
shahadat-mso <sahadat.mso@gmail.com>
2022-08-28 18:04:12 +0600

    mod

--- CP1-C/00rough.c
+++ CP1-C/00rough.c
@@ -3,9 +3,29 @@ int main()
 {
     int num=0;
 
-    while(num<10)
+    /* while(num<10)
     {
         printf("%d\n", num);
+        num++;
+    } */
+    /* while(num<5)
+    {
+        if(num==2)
+        {
+            break;
+        }
+        printf("%d Hola!\n",num);
+        num++;
+    } */
+    while (num<8)
+    {
+        if(num==4)
+        {
+            num++;
+            printf("Hello World %d\n", num);
+            continue;
+        }
+        printf("Doom %d\n", num);
         num++;
     }
 
