2c0456917a19ab866aa21b4943f471df17de58bf
shahadat-mso <sahadat.mso@gmail.com>
2022-08-28 17:23:09 +0600

    mod

--- CP1-C/00rough.c
+++ CP1-C/00rough.c
@@ -1,7 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    
+    int num;
+
+    printf("enter a number: ");
+    scanf("%d", &num);
+
+    if(num>0)
+    {
+        printf("Number is greater than 0.");
+    }
+    else if(num<0)
+    {
+        printf("Number is lower than 0.");
+    }
+    else
+    {
+        printf("The number is zero");
+    }
 
     return 0;
 }
