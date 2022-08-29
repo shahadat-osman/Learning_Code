f6763c9ab8e3b8febbb7fb4c8fe8cd5fb853bc51
shahadat-mso <sahadat.mso@gmail.com>
2022-08-30 01:28:54 +0600

    mod

--- CP1-C/4/4-03.c
+++ CP1-C/4/4-03.c
@@ -1,12 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int n =1;
+    int n =1, n1;
 
-    while (n<=11)
+    while (n<=100)
     {
         printf("%d\n", n);
         n++;
+        if(n>10)
+        {
+            break;
+        }
     }
+    while (n1<=100)
+    {
+        if(n1>10)
+        {
+            break;
+        }
+        printf(".%d\n", n1);
+        n1++;
+    }
+    
     return 0;
 }
