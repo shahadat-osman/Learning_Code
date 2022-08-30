f6cf8c328fd88f52e89af49934381703bad8b349
shahadat-mso <sahadat.mso@gmail.com>
2022-08-31 00:16:34 +0600

    mod

--- TY-C/0rough.c
+++ TY-C/0rough.c
@@ -1,18 +1,6 @@
 #include<stdio.h>
 int main()
 {
-    int num;
-
-    printf("Enter a number:\n");
-    scanf("%d", &num);
-
-    if(num%2==0)
-    {
-        printf("The number is even.\n");
-    }
-    if(num%2==1)
-    {
-        printf("The number is odd.\n");
-    }
+    
     return 0;
 }
