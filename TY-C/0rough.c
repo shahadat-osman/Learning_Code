cd60c7212dfc346d12b057d13a1df64ad42c1d4e
shahadat-mso <sahadat.mso@gmail.com>
2022-08-31 00:28:24 +0600

    mod

--- TY-C/0rough.c
+++ TY-C/0rough.c
@@ -1,19 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n1, n2;
-
-    printf("Enter two numbers:\n");
-    scanf("%d%d", &n1, &n2);
-
-    if(n2==0)
-    {
-        printf("Cannot divide by zero.\n");
-    }
-    else
-    {
-        printf("Answer is %d", n1/n2);
-    }
+    
 
     return 0;
 }
