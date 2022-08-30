c93756bfd1f1c4d356506d7c3424daa89fa3f707
shahadat-mso <sahadat.mso@gmail.com>
2022-08-31 00:56:17 +0600

    mod

--- TY-C/0rough.c
+++ TY-C/0rough.c
@@ -1,7 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int 
+    int i, j, choice;
+
+    printf("Enter two numbers:\n");
+    scanf("%d%d", &i, &j);
+
+    printf("1.Multiple the numbers.\n2.Sum the numbers.\n");
+    printf("Enter choice\n");
+    scanf("%d", &choice);
+
+    if(choice==1)
+    {
+        printf("Answer is %d\n", i*j);
+    }
+    else if(choice==2)
+    {
+        printf("Answer is %d\n", i+j);
+    }
+    else
+    {
+        printf("Wrong choice.\n");
+    }
 
     return 0;
 }
