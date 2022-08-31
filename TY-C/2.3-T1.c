866446063241157836b650a6bdc3a18f654c6717
shahadat-mso <sahadat.mso@gmail.com>
2022-08-31 21:19:20 +0600

    init

---
+++ TY-C/2.3-T1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n1, n2, answer, choice;
+
+    printf("1.Add two numbers.\n2.Substracts two numbers.\n");
+    printf("Enter Choice-");
+    scanf("%d", &choice);
+
+    if(choice==1)
+    {
+        printf("Enter two numbers for sum:\n");
+        scanf("%d %d", &n1, &n2);
+        printf("Sum is: %d\n", n1+n2);
+    }
+    else if(choice==2)
+    {
+        printf("Enter two numbers for substract.");
+        scanf("%d %d", &n1, &n2);
+        printf("Substract is: %d\n", n1-n2);
+    }
+    else
+    {
+        printf("Wrong choice.");
+    }
+    return 0;
+}
