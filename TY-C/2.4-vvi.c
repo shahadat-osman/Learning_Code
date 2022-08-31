0ebeba5ac0ec9f37334c6843829875972262527c
shahadat-mso <sahadat.mso@gmail.com>
2022-08-31 23:55:58 +0600

    init

--- TY-C/2.4-vvi.c
+++ TY-C/2.4-vvi.c
@@ -5,15 +5,12 @@ int main()
     sum=0;
     prod=1;
 
-    for(num=10; num>6; num--)
+    for(num=1; num<6; num++)
     {
         sum=sum+num;
         prod=prod*num;
     }
     printf("product and sum: %d %d\n", prod ,sum);
-
-    h=prod+h;
-    printf("%d", h);
     
     return 0;
 }
