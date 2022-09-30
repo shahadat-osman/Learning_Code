831219d994c93b89768df63771d9c935e91fe4fc
shahadat-mso <sahadat.mso@gmail.com>
2022-09-30 11:09:21 +0600

    init

---
+++ Versity/Lab312/array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, number[5];
+
+    cout<<"Enter five numbers\n";
+
+    for(i=0; i<5; i++)
+    
+    cin>>number[i];
+    cout<<"The numbers are: ";
+
+    for(i=0; i<5; i++)
+
+    cout<<number[i]<<" ";
+}
