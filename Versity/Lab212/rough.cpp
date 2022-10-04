6430a42a572fefc3276c2b3d573035183af50233
shahadat-mso <sahadat.mso@gmail.com>
2022-10-04 22:12:09 +0600

    mod

---
+++ Versity/Lab212/rough.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class myclass
+{
+    int a;
+    public:
+    myclass(); //constructor
+    void show();
+
+};
+
+myclass::myclass()
+{
+    cout<<"In constructor\n";
+    a=10;
+}
+
+void myclass::show()
+{
+    cout<<a<<"\n";
+}
+int main()
+{
+    myclass ob;
+    ob.show();
+
+    return 0;
+}
