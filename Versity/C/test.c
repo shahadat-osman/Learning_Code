f7d76c844d69d104dfbc83d1e71d54f186a551bc
shahadat-mso <sahadat.mso@gmail.com>
2022-10-08 11:59:51 +0600

    mod

--- Versity/C/test.c
+++ Versity/C/test.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello World!");
+    int var = 45;
+/* 
+    printf("var + 5 = %d\n", var + 5);
+    printf("var - 5 = %d\n", var - 5);
+    printf("var * 5 = %d\n", var * 5);
+    printf("var / 5 = %d\n", var / 5);
+    printf("var = %d\n", var);
+ */
+    int a = 10;
+    int b = 20;
 
+    printf("%d + %d = %d\n", a, b, a+b);
+    printf("%d - %d = %d\n", a, b, a-b);
+    printf("%d * %d = %d\n", a, b, a*b);
+    printf("%d / %d = %d\n", a, b, a/b);
     return 0;
 }
