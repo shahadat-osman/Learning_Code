1094c0bc81e34741960d8facb867d4806628a6d9
shahadat-mso <sahadat.mso@gmail.com>
2022-10-17 20:53:33 +0600

    mod

---
+++ Versity/Lab114/avg.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i=1, marks;
+    float t_marks=0, avg;
+
+    printf("Enter Marks:\n");
+    while(i<=10)
+    {
+        scanf("%d", &marks);
+        t_marks=marks+t_marks;
+        i++;
+    }
+    avg=t_marks/(i-1);
+    printf("Total marks = %.2f\n", t_marks);
+    printf("Average marks = %.2f", avg);
+    return 0;
+}
