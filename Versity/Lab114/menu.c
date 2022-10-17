0cd647f0a28b21052766dac9c0bf6c93c8df46cc
shahadat-mso <sahadat.mso@gmail.com>
2022-10-17 20:12:35 +0600

    mod

--- Versity/Lab114/menu.c
+++ Versity/Lab114/menu.c
@@ -1,62 +1,29 @@
 #include<stdio.h>
-int ifFunc();
-
+int main()
+{
     int num1, num2, choice;
 
-int main()
-{   
-    printf("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n");
-    printf("Enter your choice from above-\nEx:(1/2/3/4)::\n");
+    printf("Press \"1\" for Addition.\nPress \"2\" for Substraction.\nPress \"3\" for Multiplication.\nPress \"4\" for Division.\n");
     scanf("%d", &choice);
-    ifFunc();
-
-    scanf("%d\n%d", &num1, &num2);
-
-    
-
+    printf("Enter two Numbers:\n");
+    scanf("%d%d", &num1, &num2);
     switch(choice)
     {
         case 1:
-        printf("%d + %d = %d", num1, num2, num1+num2);
+        printf("The sum is %d + %d=%d", num1, num2, num1+num2);
         break;
-
         case 2:
-        printf("%d - %d = %d", num1, num2, num1-num2);
+        printf("The Substraction is %d - %d= %d", num1, num2, num1-num2);
         break;
-        
         case 3:
-        printf("%d * %d = %d", num1, num2, num1*num2);
+        printf("The Multiplication is %d*%d = %d", num1, num2, num1*num2);
         break;
-
         case 4:
-        printf("%d / %d = %d", num1, num2, num1/num2);
+        printf("The substraction is %d/%d = %d", num1, num2, num1/num2);
         break;
-        
         default:
-        printf("Wrong choice!");
-    }
+        printf("Invalid Output.");
 
-    return 0;
-}
-int ifFunc()
-{
-    if(choice==1)
-    {
-        printf("Enter two numbers for Additon:\n");
     }
-
-    if(choice==2)
-    {
-        printf("Enter two numbers for Subtraction:\n");
-    }
-    if(choice==3)
-    {
-        printf("Enter two numbers for Multiplication:\n");
-    }
-    if(choice==4)
-    {
-        printf("Enter two numbers for Division:\n");
-    }
-
     return 0;
 }
