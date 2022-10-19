eff0faf2239fc81ad05cca0f16897688d878701b
shahadat-mso <sahadat.mso@gmail.com>
2022-10-19 19:59:43 +0600

    mod

--- Versity/Lab114/menu-ext.c
+++ Versity/Lab114/menu-ext.c
@@ -1,42 +1,56 @@
 #include<stdio.h>
 int ifFunc();
-int defaultfunc();
 
-    int  choice;
-
+    int num1, num2, choice;
+    char try;
+    
 int main()
 {   
-    int num1, num2;
-
     printf("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n");
     printf("Enter your choice from above-\nEx:(1/2/3/4)::\n");
     scanf("%d", &choice);
-
-    ifFunc();
-
-    scanf("%d\n%d", &num1, &num2);
-
-    switch(choice)
+    if(choice<1 || choice>4)
     {
-        case 1:
-        printf("%d + %d = %d", num1, num2, num1+num2);
-        break;
+        printf("Wrong choice!\n");
+        printf("Try again?(Y/N):\n");
+        scanf(" %c", &try);
 
-        case 2:
-        printf("%d - %d = %d", num1, num2, num1-num2);
-        break;
+        if(try=='Y'|| try=='y')
+            return main();
+        else if(try=='N'|| try=='n')
+            printf("Thank You\n");
+    }
+    else
+    {
+        ifFunc();
+
+        scanf("%d\n%d", &num1, &num2);
+
+        switch(choice)
+        {
+            case 1:
+            printf("%d + %d = %d", num1, num2, num1+num2);
+            break;
+
+            case 2:
+            printf("%d - %d = %d", num1, num2, num1-num2);
+            break;
         
-        case 3:
-        printf("%d * %d = %d", num1, num2, num1*num2);
-        break;
+            case 3:
+            printf("%d * %d = %d", num1, num2, num1*num2);
+            break;
 
-        case 4:
+            case 4:
             if(num2==0)
-                printf("Cannot Divide by zero\n");
+            printf("Cannot Divide by zero\n");
             else
-                printf("%d / %d = %d\n", num1, num2, num1/num2);
-                printf("%d %% %d = %d\n", num1, num2, num1%num2);
-        break;
+            printf("%d / %d = %d\n", num1, num2, num1/num2);
+            printf("%d %% %d = %d\n", num1, num2, num1%num2);
+            break;
+        
+            default:
+            printf("Wrong choice!");
+        }
     }
     return 0;
 }
@@ -46,39 +60,18 @@ int ifFunc()
     {
         printf("Enter two numbers for Additon:\n");
     }
-    else if(choice==2)
+
+    if(choice==2)
     {
         printf("Enter two numbers for Subtraction:\n");
     }
-    else if(choice==3)
+    if(choice==3)
     {
         printf("Enter two numbers for Multiplication:\n");
     }
-    else if(choice==4)
+    if(choice==4)
     {
         printf("Enter two numbers for Division:\n");
     }
-    else
-    defaultfunc();
-    return 0;
-}
-int defaultfunc()
-{
-    char try;
-
-    printf("Wrong choice!\n");
-    printf("Try again?(Y/N):\n");
-    scanf(" %c", &try);
-
-    if(try=='Y'|| try=='y')
-    {
-        return main();
-    }
-    else if(try=='N'|| try=='n')
-    {
-        printf("Thank You\n");
-    }
-    else
-        defaultfunc();
     return 0;
 }
